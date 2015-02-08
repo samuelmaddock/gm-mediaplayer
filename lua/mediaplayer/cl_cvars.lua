e295761374fd1b9aab022b5786551a8ec4b08892
samuelmaddock <samuel.maddock@gmail.com>
2015-02-08 02:22:28 -0500

    Disabled fullscreen cvar saving.

--- lua/mediaplayer/cl_cvars.lua
+++ lua/mediaplayer/cl_cvars.lua
@@ -1,4 +1,4 @@
 MediaPlayer.Cvars.Resolution	= CreateClientConVar( "mediaplayer_resolution", 480, true, false )
 MediaPlayer.Cvars.Volume		= CreateClientConVar( "mediaplayer_volume", 0.15, true, false )
 MediaPlayer.Cvars.MuteUnfocused	= CreateClientConVar( "mediaplayer_mute_unfocused", 1, true, false )
-MediaPlayer.Cvars.Fullscreen	= CreateClientConVar( "mediaplayer_fullscreen", 0, true, false )
+MediaPlayer.Cvars.Fullscreen	= CreateClientConVar( "mediaplayer_fullscreen", 0, false, false )
