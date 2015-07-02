f7536f5dc68db0b575463475ef27f85b3de3b7b7
samuelmaddock <samuel.maddock@gmail.com>
2015-07-02 00:31:15 -0400

    Updated YouTube service to use the newer IFrame url.

--- lua/mediaplayer/services/youtube/cl_init.lua
+++ lua/mediaplayer/services/youtube/cl_init.lua
@@ -8,90 +8,7 @@ DEFINE_BASECLASS( "mp_service_browser" )
 
 -- https://developers.google.com/youtube/player_parameters
 -- TODO: add closed caption option according to cvar
-SERVICE.VideoUrlFormat = "https://www.youtube.com/embed/%s?enablejsapi=1&version=3&playerapiid=ytplayer&autoplay=1&controls=0&modestbranding=1&rel=0&showinfo=0"
-
--- YouTube player API
--- https://developers.google.com/youtube/js_api_reference
-local JS_Init = [[
-if (window.MediaPlayer === undefined) {
-	window.MediaPlayer = (function(){
-		var playerId = '%s',
-			timed = %s;
-
-		return {
-
-			init: function () {
-				if (this.player) return;
-
-				var player = document.getElementById(playerId);
-
-				if (!player) {
-					player = document.querySelector('embed');
-				}
-
-				this.player = player;
-
-				if (!this.player) {
-					console.error('Unable to find YouTube player element!');
-					return false;
-				}
-			},
-
-			isPlayerReady: function () {
-				return ( this.ytReady ||
-					(this.player && (this.player.setVolume !== undefined)) );
-			},
-
-			setVolume: function ( volume ) {
-				if (!this.isPlayerReady()) return;
-				this.player.setVolume(volume);
-			},
-
-			play: function () {
-				if (!this.isPlayerReady()) return;
-				this.player.playVideo();
-			},
-
-			pause: function () {
-				if (!this.isPlayerReady()) return;
-				this.player.pauseVideo();
-			},
-
-			seek: function ( seekTime ) {
-				if (!this.isPlayerReady()) return;
-				if (!timed) return;
-
-				var state, curTime, duration, diffTime;
-
-				state = this.player.getPlayerState();
-
-				/*if (state < 0) {
-					this.player.playVideo();
-				}*/
-
-				if (state === 3) return;
-
-				duration = this.player.getDuration();
-				if (seekTime > duration) return;
-
-				curTime = this.player.getCurrentTime();
-				diffTime = Math.abs(curTime - seekTime);
-				if (diffTime < 5) return;
-
-				this.player.seekTo(seekTime, true);
-			}
-
-		};
-	})();
-
-	MediaPlayer.init();
-}
-
-window.onYouTubePlayerReady = function (playerId) {
-	MediaPlayer.ytReady = true;
-	MediaPlayer.init();
-};
-]]
+SERVICE.VideoUrlFormat = htmlBaseUrl .. "youtube.html"
 
 local JS_SetVolume = "if(window.MediaPlayer) MediaPlayer.setVolume(%s);"
 local JS_Seek = "if(window.MediaPlayer) MediaPlayer.seek(%s);"
@@ -130,26 +47,11 @@ function SERVICE:OnBrowserReady( browser )
 		return
 	end
 
-	if not self._setupBrowser then
-
-		-- This doesn't always get called in time, but it's a nice fallback
-		browser:AddFunction( "window", "onYouTubePlayerReady", function( playerId )
-			if not playerId then return end
-			self.playerId = string.JavascriptSafe( playerId )
-
-			-- Initialize JavaScript MediaPlayer interface
-			local jsinit = JS_Init:format( self.playerId, self:IsTimed() )
-			browser:RunJavascript( jsinit )
-
-			YTSetVolume( self )
-		end )
-
-		self._setupBrowser = true
-
-	end
-
 	local videoId = self:GetYouTubeVideoId()
-	local url = self.VideoUrlFormat:format( videoId )
+	local timedParam = self:IsTimed() and '1' or '0'
+	local url = self.VideoUrlFormat .. '?v=' .. videoId ..
+				'&timed=' .. timedParam
+
 	local curTime = self:CurrentTime()
 
 	-- Add start time to URL if the video didn't just begin
@@ -157,18 +59,7 @@ function SERVICE:OnBrowserReady( browser )
 		url = url .. "&start=" .. math.Round(curTime)
 	end
 
-	-- Trick the embed page into thinking the referrer is youtube.com; allows
-	-- playing some restricted content due to the block by default behavior
-	-- described here: http://stackoverflow.com/a/13463245/1490006
-	url = urllib.escape(url)
-	url = htmlBaseUrl .. "redirect.html#" .. url
-
 	browser:OpenURL(url)
-
-	-- Initialize JavaScript MediaPlayer interface
-	local playerId = "player1"
-	local jsinit = JS_Init:format( playerId, self:IsTimed() )
-	browser:QueueJavascript( jsinit )
 
 end
 
