3036c865cf59ed367c2288a88a3f078d120f1ba1
samuelmaddock <samuel.maddock@gmail.com>
2015-11-01 17:54:14 -0500

    Added mediaplayer_tv entity preview to resources list.

--- lua/entities/mediaplayer_tv/shared.lua
+++ lua/entities/mediaplayer_tv/shared.lua
@@ -3,6 +3,7 @@ AddCSLuaFile()
 if SERVER then
 	resource.AddFile( "models/gmod_tower/suitetv_large.mdl" )
 	resource.AddFile( "materials/models/gmod_tower/suitetv_large.vmt" )
+	resource.AddSingleFile( "materials/entities/mediaplayer_tv.png" )
 end
 
 ENT.PrintName 		= "Media Player TV"
