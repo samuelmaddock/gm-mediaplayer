516bf6b10a1646388bef0d5b2ee34da83167fbe7
samuelmaddock <samuel.maddock@gmail.com>
2015-11-01 19:07:46 -0500

    Redesigned 'no media playing' idlescreen.

--- lua/mediaplayer/cl_idlescreen.lua
+++ lua/mediaplayer/cl_idlescreen.lua
@@ -5,74 +5,54 @@ local DefaultIdlescreen = [[
 	<meta charset="utf-8">
 	<title>MediaPlayer Idlescreen</title>
 	<style type="text/css">
-	.repeating-grid {
-	  z-index: -1;
-	  position: absolute;
-	  top: 0;
-	  bottom: 0;
-	  left: 0;
-	  right: 0;
-	  background-size: 120px 120px;
-	  background-image: -webkit-repeating-linear-gradient(0deg, rgba(0, 0, 0, 0.08), rgba(0, 0, 0, 0.08) 2px, transparent 2px, transparent 120px), -webkit-repeating-linear-gradient(-90deg, rgba(0, 0, 0, 0.08), rgba(0, 0, 0, 0.08) 2px, transparent 2px, transparent 120px), -webkit-repeating-linear-gradient(0deg, rgba(0, 0, 0, 0.08), rgba(0, 0, 0, 0.08) 1px, transparent 1px, transparent 30px), -webkit-repeating-linear-gradient(-90deg, rgba(0, 0, 0, 0.08), rgba(0, 0, 0, 0.08) 1px, transparent 1px, transparent 30px);
-	}
-	.background-example {
-	  opacity: 0.66;
-	  z-index: -2;
-	  position: absolute;
-	  top: 0;
-	  bottom: 0;
-	  left: 0;
-	  right: 0;
-	  background: #6e00ff;
-	  /* Old browsers */
-	  background: -moz-linear-gradient(-45deg, #6e00ff 0%, #ff7700 100%);
-	  background: -webkit-gradient(linear, left top, right bottom, color-stop(0%, #6e00ff), color-stop(100%, #ff7700));
-	  /* Chrome,Safari4+ */
-	  background: -webkit-linear-gradient(-45deg, #6e00ff 0%, #ff7700 100%);
-	  /* Chrome10+,Safari5.1+ */
-	  background: -o-linear-gradient(-45deg, #6e00ff 0%, #ff7700 100%);
-	  /* Opera 11.10+ */
-	  background: -ms-linear-gradient(-45deg, #6e00ff 0%, #ff7700 100%);
-	  /* IE10+ */
-	  background: linear-gradient(135deg, #6e00ff 0%, #ff7700 100%);
-	  /* W3C */
-	  filter: progid:DXImageTransform.Microsoft.gradient(startColorstr='#6e00ff', endColorstr='#ff7700', GradientType=1);
-	  /* IE6-9 fallback on horizontal gradient */
+	html, body {
+		margin: 0;
+		padding: 0;
+		width: 100%;
+		height: 100%;
 	}
 	html {
 		background: #fff;
 	}
-	html,
 	body {
-	  margin: 0;
-	  padding: 0;
-	  width: 100%;
-	  height: 100%;
-	  overflow: hidden;
-	  display: -webkit-box;
-	  display: -moz-box;
-	  display: box;
-	  -webkit-box-orient: horizontal;
-	  -moz-box-orient: horizontal;
-	  -box-orient: horizontal;
-	  -webkit-box-pack: center;
-	  -webkit-box-align: center;
+		overflow: hidden;
+		display: -webkit-box;
+		display: -moz-box;
+		display: box;
+		-webkit-box-orient: horizontal;
+		-moz-box-orient: horizontal;
+		-box-orient: horizontal;
+		-webkit-box-pack: center;
+		-webkit-box-align: center;
+		background: -webkit-radial-gradient(center, ellipse cover,
+			transparent 0%, rgba(0, 0, 0, 0.7) 100%);
+	}
+	h1 {
+		margin: 0;
+		padding: 0;
+	}
+	.background {
+		position: absolute;
+		display: block;
+		width: 100%;
+		z-index: -1;
+		-webkit-filter: blur(8px);
+		-webkit-transform: scale(1.2);
+		opacity: 0.66;
 	}
 	.content {
-	  color: rgba(0, 0, 0, 0.66);
-	  font-family: sans-serif;
-	  font-size: 1.5em;
-	  text-align: center;
-	  -webkit-box-flex: 1;
-	  -moz-box-flex: 1;
-	  box-flex: 1;
+		color: rgba(255, 255, 255, 0.66);
+		font-family: sans-serif;
+		font-size: 1.5em;
+		text-align: center;
+		-webkit-box-flex: 1;
+		-moz-box-flex: 1;
+		box-flex: 1;
 	}
 	</style>
 </head>
 <body>
-	<div class="repeating-grid"></div>
-	<div class="background-example"></div>
-
+	<img src="asset://mapimage/gm_construct" class="background">
 	<div class="content">
 	  <h1>No media playing</h1>
 	  <p>Right-click on the media player to see a list of available actions</p>
