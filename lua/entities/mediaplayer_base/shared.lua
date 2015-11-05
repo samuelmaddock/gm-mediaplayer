a308e1a573d8e9ebfa4f9af88385152f7f9e175d
samuelmaddock <samuel.maddock@gmail.com>
2015-11-05 02:59:22 -0500

    Media player queue is now persisted properly on entities.

--- lua/entities/mediaplayer_base/shared.lua
+++ lua/entities/mediaplayer_base/shared.lua
@@ -81,3 +81,33 @@ end
 function ENT:UpdateTransmitState()
 	return TRANSMIT_PVS
 end
+
+function ENT:OnEntityCopyTableFinish( data )
+	local mp = self:GetMediaPlayer()
+	local queue = table.Copy( mp:GetMediaQueue() )
+
+	local media = mp:GetMedia()
+	if media then
+		table.insert( queue, 1, table.Copy( media ) )
+	end
+
+	data.MediaPlayerPersistData = {
+		queue = queue
+	}
+
+	data._mp = nil
+end
+
+function ENT:PostEntityPaste( ply, ent, createdEnts )
+	local mpdata = self.MediaPlayerPersistData
+	local mp = self:GetMediaPlayer()
+
+	for _, mediaData in ipairs( mpdata.queue ) do
+		local media = MediaPlayer.GetMediaForUrl( mediaData.url )
+		if not media then continue end
+		table.Merge( media, mediaData )
+		mp:AddMedia( media )
+	end
+
+	mp:QueueUpdated()
+end
