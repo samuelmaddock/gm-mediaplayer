0656c87717f6b21c61ccc03c3ff8debc0cb2f5fa
samuelmaddock <samuel.maddock@gmail.com>
2015-11-06 22:11:59 -0500

    Added archive flag to 'mediaplayer_allow_webpages' convar.

--- lua/mediaplayer/sh_cvars.lua
+++ lua/mediaplayer/sh_cvars.lua
@@ -7,6 +7,7 @@ cvars.AddChangeCallback( "mediaplayer_debug", function(name, old, new)
 end)
 
 MediaPlayer.Cvars.AllowWebpages = CreateConVar( "mediaplayer_allow_webpages", 0, {
+	FCVAR_ARCHIVE,
 	FCVAR_NOTIFY,
 	FCVAR_REPLICATED,
 	FCVAR_SERVER_CAN_EXECUTE
