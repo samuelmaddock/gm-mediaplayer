d391e77b91f9485c54b6d292751ff9e2223cdc39
Datamats <datamats@datamats.com>
2017-05-27 12:48:58 +0200

    add ability to make entity players not use 3d audio

--- lua/mediaplayer/services/audiofile/cl_init.lua
+++ lua/mediaplayer/services/audiofile/cl_init.lua
@@ -41,7 +41,7 @@ function SERVICE:Play()
 		local settings = table.Copy(self.StreamOptions)
 
 		-- .ogg files can't seem to use 3d?
-		if Audio3DCvar:GetBool() and IsValid(self.Entity) and
+		if Audio3DCvar:GetBool() and IsValid(self.Entity) and not self.Entity.Disable3DAudio and
 				not self.url:match(".ogg") then
 			table.insert(settings, "3d")
 		end
