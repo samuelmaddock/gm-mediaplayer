4ecf0504a61cf57ec694fe5bba48a2fdd15b2c54
Redox <mail@redox.to>
2023-06-24 01:52:40 +0200

    Fix error

--- lua/mediaplayer/sh_metadata.lua
+++ lua/mediaplayer/sh_metadata.lua
@@ -146,7 +146,7 @@ function MediaPlayer.Metadata:Save( media )
 			string.format( "%s,", sql.SQLStr( media:Title() ) ) ..
 			string.format( "%s,", media:Duration() ) ..
 			string.format( "%s,", sql.SQLStr( media:Thumbnail() ) ) ..
-			string.format( "%s,", sql.SQLStr( util.TableToJSON(media._metadata.extra) ) ) ..
+			string.format( "%s,", sql.SQLStr( util.TableToJSON(media._metadata.extra or {}) ) ) ..
 			string.format( "%d,", os.time() ) ..
 			string.format( "%d)", os.time() )
 
